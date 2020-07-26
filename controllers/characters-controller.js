640f0388824ba475d213450978c7999d30068140
Nina Ruiz <nina.ruizandres@gmail.com>
2020-07-26 21:24:26 +0200

    Add character controller and character route file

---
+++ controllers/characters-controller.js
@@ -0,0 +1,26 @@
+const Character = require('../schemas/character');
+
+// Create character
+const createCharacter = (req, res) =>{
+
+};
+
+// Delete character
+const deleteCharacter = (req, res) =>{
+
+};
+
+// Get character
+const getCharacter = (req, res) =>{
+
+};
+
+// Update character
+const updateCharacter = (req, res) =>{
+
+};
+
+// Pagination characters
+const getCharacters = (req, res) =>{
+
+};
